1a0453b62ef5700285d6a8dcadca5a701056ea51
Everton Lira <everton_henrique113@hotmail.com>
2023-03-17 03:18:22 -0300

    Segunda alteração.

--- tipos-e-variaveis/src/TiposVariaveis.java
+++ tipos-e-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,11 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        short numeroCurto1 = 1;
+        int numeroNormal1 = numeroCurto1; // Atribuindo um valor short dentro de um tipo int - É compatível.
+        // short numeroCurto2 = numeroNormal1; // Já não é possível atribuir um valor do tipo int dentro de um tipo short, pois o tipo int é maior. Para isso, é preciso fazer uso do método "casting".
+        short numeroCurto2 = (short) numeroNormal1;
+
+        final double VALOR_DE_PI = 3.14; //Exemplo de constante.
+
     }
 }
