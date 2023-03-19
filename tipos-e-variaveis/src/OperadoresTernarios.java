105d297724cffcf8930510f846142f5f71a7e7c0
Everton Lira <everton_henrique113@hotmail.com>
2023-03-19 14:06:29 -0300

    5ª alteração - adição da class OperadoresTernarios

---
+++ tipos-e-variaveis/src/OperadoresTernarios.java
@@ -0,0 +1,30 @@
+public class OperadoresTernarios {
+    public static void main(String[] args) {
+        // Exemplo com estrutura ternária:
+
+        int a, b;
+
+        a = 6;
+        b = 6;
+
+        String resultado = a == b ?"verdadeiro" : "falso";
+        
+        System.out.println(resultado);
+
+        // Exemplo com estrutura de tomada de decisão IF/ELSE:
+
+        // int a, b;
+        // String resultado = "";
+
+        // a = 5;
+        // b = 6;
+
+        // if(a == b)
+	    //     resultado = "verdadeiro";
+        // else
+	    //     resultado = "falso";
+        
+        // System.out.println(resultado);
+
+    }
+}
