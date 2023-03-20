b0cefb4500cb5d9613413612ff4816c2f67bb4ba
Everton Lira <everton_henrique113@hotmail.com>
2023-03-20 00:54:11 -0300

    6ª edição - adição da class OperadoresRelacionais

---
+++ tipos-e-variaveis/src/OperadoresRelacionais.java
@@ -0,0 +1,43 @@
+public class OperadoresRelacionais {
+    public static void main(String[] args) {
+        
+        // Verificação de relação entre objetos.
+        
+        String nomeUm = "Everton";
+        String nomeDois = new String("Everton");
+
+        System.out.println(nomeUm.equals(nomeDois));
+
+        // -----------------------------------------------------------
+
+        // Teste de operadores relacionais com estrutura de decisão com valores numéricos.
+        // Todos os operadores relacionais podem ser utilizados na condição.
+
+        // int numero1 = 5;
+        // int numero2 = 4;
+        // boolean simOuNao;
+
+        // if (numero1 < numero2) {
+        //     System.out.println("A nossa condição é verdadeira. Número 1 é maior que o número 2.");
+        // }
+        // else {
+        //     System.out.println("A condição é falsa.");
+        // }
+
+        // -----------------------------------------------------------
+        
+        // Teste com operadores relacionais de forma mais simples.
+
+        // simOuNao = numero1 == numero2;
+        // System.out.println("O número um é igual ao número dois? " + simOuNao);
+
+        // simOuNao = numero1 != numero2;
+        // System.out.println("O número um é diferente do número dois? " + simOuNao );
+
+        // simOuNao = numero1 > numero2;
+        // System.out.println("O número um é maior que o número dois? " + simOuNao);
+
+
+
+    }
+}
